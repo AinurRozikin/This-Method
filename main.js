58614b9d8fbea882ad055f10c5a3b8f18629235f
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-12 07:49:09 +0700

    menambahkan try dan catch untuk menangani error

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/*Keyword This digunakan untuk memanggil properti dari sebuah objek
+sedangkan method adalah function yang dimiliki oleh sebuah objek  */
+
 const penggunaanMethodDanThis = {
     merek : 'toyota',
     warna : 'merah',
@@ -6,5 +9,16 @@ const penggunaanMethodDanThis = {
     }
 }
 
-/*Keyword This digunakan untuk memanggil properti dari sebuah objek
-sedangkan method adalah function yang dimiliki oleh sebuah objek  */
+
+// penggunaan try dan catch untuk menangani error
+const tryCatch = (arg) => {
+    try {
+        console.log(arg.toUpperCase());
+    } catch (error) {
+        console.log(error);
+        console.log('silahkan masukkan tipe data string pada parameter arg');
+    }
+}
+
+console.log(tryCatch('zikin'));
+console.log(tryCatch(999));
