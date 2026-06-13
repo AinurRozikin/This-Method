09b4c44fc552b57ba3edb971b1ed8759b0dc11d8
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-13 10:54:29 +0700

    menambahkan every, some, dan setTimeout (asinkronus javascript)

--- main.js
+++ main.js
@@ -22,3 +22,21 @@ const tryCatch = (arg) => {
 
 console.log(tryCatch('zikin'));
 console.log(tryCatch(999));
+
+const scores = [89, 58, 79,98,87,81,12];
+
+/* 
+untuk menghasilkan true/ false dari sebuah array
+every akan bernilai true jika semua kriteria terpenuhi sedangkan some boleh salah satu
+*/
+setTimeout(() => {
+    console.log('kode ini akan dijalankan setelah 5 detik berlalu');
+    let beberapa = scores.some((score) => score >= 70);
+    console.log(beberapa);
+}, 5000);
+
+const setiap = scores.every((score) => score >= 70);
+console.log(setiap);
+
+
+
